beb3726f9491a6a538550193090afce16c0ea3a1
unknown <alexanderwhile@gmail.com>
2020-04-28 15:33:57 -0400

    Add blackjack and hookers filler, some error handling to roll function.

--- boy.py
+++ boy.py
@@ -11,6 +11,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='/i ')
 
+
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
@@ -24,16 +25,31 @@ async def on_member_join(member):
     )
 
 
-@bot.command(name = 'roll_dice', help='Simulates rolling dice.')
-async def roll(ctx, number_of_dice: int, number_of_sides: int):
-    dice = [
-        str(random.choice(range(1, number_of_sides +1)))
-        for _ in range(number_of_dice)
-    ]
-    await ctx.send(', '.join(dice))
+@bot.command(name='roll', help='Simulates rolling dice.')
+async def roll(ctx, number_of_dice, number_of_sides):
+    try:
+        number_of_dice = int(float(number_of_dice))
+        number_of_sides = int(float(number_of_sides))
+        dice = [
+            str(random.choice(range(1, number_of_sides + 1)))
+            for _ in range(number_of_dice)
+            ]
+        await ctx.send(', '.join(dice))
+    except:
+        await ctx.send("I don't understand what you want me to do.")
 
 
-@bot.command(name = 'who_is_a_good_boy', help='Returns who is a good boy.')
+@bot.command(name='blackjack', help='Play a game of Blackjack with Ishbot!')
+async def deal(ctx):
+    await ctx.send("I don't know how to play that yet, but I will soon!")
+
+
+@bot.command(name='hookers', help="Wouldn't you like to know?")
+async def hooker(ctx):
+    await ctx.send("We don't have hookers just yet, sorry")
+
+
+@bot.command(name='who_is_a_good_boy', help='Returns who is a good boy.')
 async def who_is_a_good_boi(ctx):
     await ctx.send('*barks happily*')
 
